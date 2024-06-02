5b8f016f0742e62acd3789858b35b8fca413f09d
luoenxing <Lsw2221480288@163.com>
2024-06-02 22:46:59 +0800

    Before refactoring the functional_tests

--- lists/views.py
+++ lists/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', '')
+    })
     # return HttpResponse('<html><title>To-Do lists</title></html>')
 
 # Create your views here.
