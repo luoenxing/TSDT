fa75f21c9a301ab91f6a32d9ae9c9f2d9baf42b6
luoenxing <Lsw2221480288@163.com>
2024-06-04 17:05:19 +0800

    Show all items in template

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Item
 
+'''
 def home_page(request):
+    item = Item()
+    # 从 request.POST 字典中获取键 item_text 对应的值，并将其赋值给 item.text
+    item.text = request.POST.get('item_text', '')
+    item.save()
+
     return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', '')
+        'new_item_text': item.text if request.method == 'POST' else ''
     })
     # return HttpResponse('<html><title>To-Do lists</title></html>')
+'''
+def home_page(request):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
 
-# Create your views here.
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
